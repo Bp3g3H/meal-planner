6e604137b64106f7ebddad9429c793094c83ea3d
Lubomir <zaimpuls1@gmail.com>
2026-06-10 21:36:30 +0300

    fix: adding @Entity and changing id type from int to UUID

--- src/main/java/com/lubomirgeorgiev/meal_planner/model/entity/user/User.java
+++ src/main/java/com/lubomirgeorgiev/meal_planner/model/entity/user/User.java
@@ -7,17 +7,19 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private int id;
+    private UUID id;
     @Column(nullable = false, unique = true)
     private String username;
     private String email;
