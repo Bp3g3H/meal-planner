91006ae293d27cba36c84021aa90940c0ff7f5c1
Lubomir <zaimpuls1@gmail.com>
2026-06-15 21:37:12 +0300

    refactor: feat: implement registration and password hashing

--- src/main/java/com/lubomirgeorgiev/meal_planner/service/user/UserService.java
+++ src/main/java/com/lubomirgeorgiev/meal_planner/service/user/UserService.java
@@ -3,8 +3,12 @@ package com.lubomirgeorgiev.meal_planner.service.user;
 import com.lubomirgeorgiev.meal_planner.mapper.user.UserMapper;
 import com.lubomirgeorgiev.meal_planner.model.dto.user.RegisterDto;
 import com.lubomirgeorgiev.meal_planner.model.dto.user.UserDto;
+import com.lubomirgeorgiev.meal_planner.model.entity.group.Group;
+import com.lubomirgeorgiev.meal_planner.model.entity.user.GroupChoice;
 import com.lubomirgeorgiev.meal_planner.model.entity.user.User;
+import com.lubomirgeorgiev.meal_planner.model.entity.user.UserRole;
 import com.lubomirgeorgiev.meal_planner.repository.user.UserRepository;
+import com.lubomirgeorgiev.meal_planner.service.group.GroupService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +17,42 @@ public class UserService {
 
     private PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
+    private GroupService groupService;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, GroupService groupService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.groupService = groupService;
     }
 
 
     public UserDto register (RegisterDto registerDto) {
-        userRepository.findByUsername(registerDto.getUsername()).ifPresent(user -> {
-            throw new RuntimeException("Username is already in use");
-        });
+        if (userRepository.existsByEmail(registerDto.getEmail()) || userRepository.existsByUsername(registerDto.getUsername())) {
+            //TODO change to UserAlreadyExistsException
+            throw new RuntimeException("User already exists");
+        }
 
-        registerDto.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-        User userEntity = UserMapper.toUserEntity(registerDto);
+        if (!registerDto.getPassword().equals(registerDto.getConfirmPassword())) {
+            throw new RuntimeException("Passwords do not match");
+        }
+        Group group = null;
+        if (registerDto.getGroupChoice().equals(GroupChoice.CREATE)) {
+            group = groupService.createGroup(registerDto.getGroupName(), registerDto.getGroupPassword());
+        } else if (registerDto.getGroupChoice().equals(GroupChoice.JOIN)) {
+            group = groupService.joinGroup(registerDto.getGroupName(), registerDto.getGroupPassword());
+        } else if (registerDto.getGroupChoice().equals(GroupChoice.NONE)) {
+            group = groupService.createDummyGroup();
+        }
 
-        userRepository.save(userEntity);
-        return UserMapper.toUserDto(userEntity);
+        User user = User.builder()
+                .username(registerDto.getUsername())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
+                .email(registerDto.getEmail())
+                .role(UserRole.USER)
+                .group(group)
+                .build();
+
+        userRepository.save(user);
+        return UserMapper.toUserDto(user);
     }
 }
