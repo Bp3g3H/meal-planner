1dbaa103b97538d3f8463b034ab9fdaa9a9305fe
Lubomir <zaimpuls1@gmail.com>
2026-08-04 23:43:00 +0300

    fix: ComplianceCheckRepository switching findByExternalUserIdAndCheckDateBetween to accept LocalDate instead of date

--- meal-planner-rest/src/main/java/app/repository/compliance_check/ComplianceCheckRepository.java
+++ meal-planner-rest/src/main/java/app/repository/compliance_check/ComplianceCheckRepository.java
@@ -4,12 +4,12 @@ import app.model.entity.compliance_check.ComplianceCheck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ComplianceCheckRepository extends JpaRepository<ComplianceCheck, UUID> {
 
-    List<ComplianceCheck> findByExternalUserIdAndCheckDateBetween(UUID externalId, Date start, Date end);
+    List<ComplianceCheck> findByExternalUserIdAndCheckDateBetween(UUID externalId, LocalDate start, LocalDate end);
 }
