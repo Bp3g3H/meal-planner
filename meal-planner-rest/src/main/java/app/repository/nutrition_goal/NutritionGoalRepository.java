aa2cffaf69cf08d7ca60ca419a10dd777f9d77fc
Lubomir <zaimpuls1@gmail.com>
2026-08-04 19:27:25 +0300

    feat: add jpa repositories for nutrition entities

--- meal-planner-rest/src/main/java/app/repository/nutrition_goal/NutritionGoalRepository.java
+++ meal-planner-rest/src/main/java/app/repository/nutrition_goal/NutritionGoalRepository.java
@@ -4,8 +4,12 @@ import app.model.entity.nutrition_goal.NutritionGoal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NutritionGoalRepository extends JpaRepository<NutritionGoal, UUID> {
+    Optional<NutritionGoal> findByExternalUserId(UUID externalId);
+    boolean existsByExternalUserId(UUID externalId);
+
 }
