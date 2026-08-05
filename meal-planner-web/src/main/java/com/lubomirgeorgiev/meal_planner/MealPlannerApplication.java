8d06d763b8c53018e85e1699169d2d617709a79f
Lubomir <zaimpuls1@gmail.com>
2026-08-05 20:55:53 +0300

    chore: add feign client dependency to main application

--- meal-planner-web/src/main/java/com/lubomirgeorgiev/meal_planner/MealPlannerApplication.java
+++ meal-planner-web/src/main/java/com/lubomirgeorgiev/meal_planner/MealPlannerApplication.java
@@ -2,7 +2,9 @@ package com.lubomirgeorgiev.meal_planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MealPlannerApplication {
 
