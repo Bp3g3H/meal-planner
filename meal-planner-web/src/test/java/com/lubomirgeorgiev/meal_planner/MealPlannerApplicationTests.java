a3b2c46c39af01e2280521be3d4c8c849b1cc5ff
Lubomir <zaimpuls1@gmail.com>
2026-08-08 18:43:58 +0300

    chore: add jacoco coverage plugin to both applications

--- meal-planner-web/src/test/java/com/lubomirgeorgiev/meal_planner/MealPlannerApplicationTests.java
+++ meal-planner-web/src/test/java/com/lubomirgeorgiev/meal_planner/MealPlannerApplicationTests.java
@@ -2,7 +2,9 @@ package com.lubomirgeorgiev.meal_planner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MealPlannerApplicationTests {
 
