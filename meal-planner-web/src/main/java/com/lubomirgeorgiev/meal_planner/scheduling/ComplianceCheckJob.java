2072c34c441b5ea263f3587a4816e94c3003cbf7
Lubomir <zaimpuls1@gmail.com>
2026-08-09 21:26:51 +0300

    chore: adding logs to schedule job

--- meal-planner-web/src/main/java/com/lubomirgeorgiev/meal_planner/scheduling/ComplianceCheckJob.java
+++ meal-planner-web/src/main/java/com/lubomirgeorgiev/meal_planner/scheduling/ComplianceCheckJob.java
@@ -3,17 +3,20 @@ package com.lubomirgeorgiev.meal_planner.scheduling;
 import com.lubomirgeorgiev.meal_planner.holder.TrendingDishHolder;
 import com.lubomirgeorgiev.meal_planner.model.dto.compliance_check.ComplianceCheckRequest;
 import com.lubomirgeorgiev.meal_planner.model.dto.nutrition_goal.NutritionGoalResponse;
-import com.lubomirgeorgiev.meal_planner.model.dto.user.UserDto;
 import com.lubomirgeorgiev.meal_planner.model.entity.dish.Dish;
+import com.lubomirgeorgiev.meal_planner.model.entity.user.User;
 import com.lubomirgeorgiev.meal_planner.service.meal_log.MealLogService;
 import com.lubomirgeorgiev.meal_planner.service.nutrition.NutritionGoalClientService;
 import com.lubomirgeorgiev.meal_planner.service.nutrition.client.NutritionServiceClient;
 import com.lubomirgeorgiev.meal_planner.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Slf4j
 @Component
 public class ComplianceCheckJob {
 
@@ -34,7 +37,11 @@ public class ComplianceCheckJob {
 
     @Scheduled(cron = "0 5 0 * * *")
     public void runDailyComplianceCheck() {
-        for (UserDto user : userService.getAllUsers()) {
+        log.info("runDailyComplianceCheck started");
+        int count = 0;
+        List<User> users = userService.findAll();
+
+        for (User user : users) {
             NutritionGoalResponse goal = nutritionGoalClientService.getGoalOrNull(user.getId());
 
             if (goal == null) {
@@ -43,20 +50,27 @@ public class ComplianceCheckJob {
 
             LocalDate yesterday = LocalDate.now().minusDays(1);
             int consumed = mealLogService.getTotalCaloriesForUserOnDate(user.getId(), yesterday);
+            log.info("user {} consumed {} calories consumed", user.getId(), consumed);
             ComplianceCheckRequest  complianceCheckRequest= ComplianceCheckRequest.builder()
                     .externalUserId(user.getId())
                     .checkDate(yesterday)
                     .totalCaloriesConsumed(consumed)
-                    .targetedCalories(goal.getDailyCalorieTarget()).build();
+                    .targetedCalories(goal.getDailyCalorieTarget())
+                    .build();
 
             nutritionServiceClient.recordComplianceCheck(complianceCheckRequest);
+            count++;
         }
+
+        log.info("runDailyComplianceCheck finished. Number of compliance checks in daily compliance checks are {}", count);
     }
 
     @Scheduled(fixedRate = 1_800_000)
     public void refreshTrendingDish() {
+        log.info("refreshTrendingDish started");
         Dish trendingDish = mealLogService.getMostPopularDishForWeek();
 
         trendingDishHolder.setTrendingDish(trendingDish);
+        log.info("refreshTrendingDish finished. Trending dish has been refreshed");
     }
 }
